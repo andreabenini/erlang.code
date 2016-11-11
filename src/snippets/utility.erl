efcdbdd05036aea0e038aceb716eeef0b0277b7c
Ben <andreabenini@users.noreply.github.com>
2016-11-11 18:04:25 +0100

    Update utility.erl

--- src/snippets/utility.erl
+++ src/snippets/utility.erl
@@ -1,3 +1,6 @@
 
 %% useful defines
 -define(FUN_NAME,  element(2, element(2, process_info(self(), current_function)))).     %% Return current function name
+
+%% list of all loaded modules in erlang
+code:all_loaded()
