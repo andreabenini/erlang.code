9f2e47b0135bb27210bdbe230df5cb92c2fb63ad
Ben <andreabenini@users.noreply.github.com>
2016-11-14 14:39:16 +0100

    Update utility.erl

--- src/snippets/utility.erl
+++ src/snippets/utility.erl
@@ -2,5 +2,8 @@
 %% useful defines
 -define(FUN_NAME,  element(2, element(2, process_info(self(), current_function)))).     %% Return current function name
 
+%% Just show us where the errors are...
+-define(PRINT_STACK_TRACE, erlang:get_stacktrace()).
+
 %% list of all loaded modules in erlang
 code:all_loaded()
