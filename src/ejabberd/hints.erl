9c2298a358a6fb382c021dbe2c40a80b62c70191
Ben <andreabenini@users.noreply.github.com>
2017-01-11 10:28:26 +0100

    get conference host
    Getting conference host domain from xmpp domain

--- src/ejabberd/hints.erl
+++ src/ejabberd/hints.erl
@@ -1,2 +1,10 @@
 %% List of online rooms (for every domain in the current host)
-ets:tab2list(muc_online_room)
+ets:tab2list(muc_online_room).
+
+%% Getting conference host from XMPP domain
+%% @param Host (binary) the XMPP domain. ie: <<"domain.com">>
+%% @return ConferenceHost (binary) Something like: <<"conference.domain.com">>
+getConferenceHost(Host) ->
+    XMPPDomain = jid:nameprep(Host),
+    Module = gen_mod:db_mod(XMPPDomain, mod_muc),
+    gen_mod:get_module_opt_host(Host, mod_muc, <<"conference.@HOST@">>).
