162c2fb517e1973b7b759bbd4d62c8ef57a815a0
Ben <andreabenini@users.noreply.github.com>
2017-01-12 10:56:52 +0100

    Update utility.erl

--- src/snippets/utility.erl
+++ src/snippets/utility.erl
@@ -7,3 +7,6 @@
 
 %% list of all loaded modules in erlang
 code:all_loaded()
+
+%% Check if a function is defined: true|false (http://erlang.org/doc/man/erlang.html#function_exported-3)
+erlang:function_exported(io, fwrite, 2).
