876130e0b30f9e363a2b69d5c8de749c56a077d9
Ben <andreabenini@users.noreply.github.com>
2017-01-12 09:29:59 +0100

    Update hints.erl

--- src/ejabberd/hints.erl
+++ src/ejabberd/hints.erl
@@ -5,6 +5,4 @@ ets:tab2list(muc_online_room).
 %% @param Host (binary) the XMPP domain. ie: <<"domain.com">>
 %% @return ConferenceHost (binary) Something like: <<"conference.domain.com">>
 getConferenceHost(Host) ->
-    XMPPDomain = jid:nameprep(Host),
-    Module = gen_mod:db_mod(XMPPDomain, mod_muc),
     gen_mod:get_module_opt_host(Host, mod_muc, <<"conference.@HOST@">>).
