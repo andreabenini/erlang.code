a54375377bbc9a33850e95fd447c1a4c5dffe7b6
Ben <andreabenini@users.noreply.github.com>
2017-01-12 11:44:22 +0100

    Update utility.erl

--- src/snippets/utility.erl
+++ src/snippets/utility.erl
@@ -10,3 +10,8 @@ code:all_loaded()
 
 %% Check if a function is defined: true|false (http://erlang.org/doc/man/erlang.html#function_exported-3)
 erlang:function_exported(io, fwrite, 2).
+%% ...even better:
+%% (credits: gootik @slack channel)
+is_defined({M, F, A}) ->
+    code:load_file(M),
+    erlang:function_exported(M, F, A).
