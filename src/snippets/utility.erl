81fd5b43edcc31b67a89e86623454a4b53fed6a6
Ben <andreabenini@users.noreply.github.com>
2017-01-13 09:23:39 +0100

    bindateToTimestamp
    Convert a string datetime to a unix timestamp

--- src/snippets/utility.erl
+++ src/snippets/utility.erl
@@ -15,3 +15,14 @@ erlang:function_exported(io, fwrite, 2).
 is_defined({M, F, A}) ->
     code:load_file(M),
     erlang:function_exported(M, F, A).
+
+%% Return a unix timestamp from a datetime string
+%% @param DateTime (binary) A binary datetime (example from PostgreSQL: <<"2017-01-09 12:31:48.250951">>)
+%% @return (integer) a valid unix datetime
+bindateToTimestamp(DateTime) ->
+    DateStr = binary_to_list(DateTime),                     %% Field  : "2017-01-09 12:31:48.250951"
+    List    = [{1,4},{6,7},{9,10},{12,13},{15,16},{18,19}], %% Format : YYYY,MM,DD,HH,mm,SS        (Field Format)
+    [Year,Month,Day,Hour,Minute,Second] = [list_to_integer(string:sub_string(DateStr, X, Y)) || {X,Y} <- List],
+    UNIXTimeStamp = calendar:datetime_to_gregorian_seconds({{Year, Month, Day}, {Hour, Minute, Second}}) -
+                    calendar:datetime_to_gregorian_seconds({{1970, 1, 1}, {0, 0, 0}}),
+    UNIXTimeStamp.
