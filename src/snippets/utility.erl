b9ced1d904120f73879646012cae2d09f07e733c
Ben <andreabenini@users.noreply.github.com>
2017-01-20 09:16:49 +0100

    errorGetLine
    ERROR GET LINE - Returns the line where the error has been generated

--- src/snippets/utility.erl
+++ src/snippets/utility.erl
@@ -5,6 +5,15 @@
 %% Just show us where the errors are...
 -define(PRINT_STACK_TRACE, erlang:get_stacktrace()).
 
+%% ERROR GET LINE - Returns the line where the error has been generated
+%% @param FunctionName (atom) Name of the function to search in the stack trace [mostly ?FUN_NAME]
+%% @return (binary) Error line number as binary type
+errorGetLine(FunctionName) ->
+    StackFunctionError = lists:keysearch(FunctionName, 2, erlang:get_stacktrace()),
+    {value, {_, _, _, LineInfo} } = StackFunctionError,                         %% {value,{mod_restapi,chatSend,3, [{file,"src/mod_restapi.erl"},{line,1203}]}}
+    {value, {line, LineError}} = lists:keysearch(line, 1, LineInfo),            %% {value,{line,1203}}
+    integer_to_binary(LineError).
+
 %% list of all loaded modules in erlang
 code:all_loaded()
 
