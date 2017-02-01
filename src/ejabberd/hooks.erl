f89a373aa02889121971face8fb78e2a8c02c815
Ben <andreabenini@users.noreply.github.com>
2017-02-01 10:26:22 +0100

    user_send_packet
    Message handler for each packet sent from an user

--- src/ejabberd/hooks.erl
+++ src/ejabberd/hooks.erl
@@ -3,13 +3,23 @@ init([Host, Opts]) ->
     ...
     %% filter_packet URL must be called with global flag set
     ejabberd_hooks:add(filter_packet, global, ?MODULE, filterSomething, 50),
+
+    %% user_send_packet get raised for each single packet sent from an user
+    ejabberd_hooks:add(user_send_packet, Host, ?MODULE, myownMessageHandler, 50),
     ...
 
 terminate(_Reason, _State) ->
     ...
     ejabberd_hooks:delete(filter_packet, global, ?MODULE, filterSomething, 50),
+    
+    %% It's nice to have the XMPP Domain in _State as a module variable
+    ejabberd_hooks:delete(user_send_packet, XMPPDomain, ?MODULE, myownMessageHandler, 50),
     ...
 
 filterSomething(Packet) ->
     ?INFO_MSG("Packet is ~p", [Packet]),
+    Packet.
+
+vcardMessageHandler(Packet, C2SState, Source, Destination) ->
+    ?INFO_MSG("Source=~p, Destination=~p, State=~p, Packet=~p", [Source, Destination, C2SState, Packet]),
     Packet.
