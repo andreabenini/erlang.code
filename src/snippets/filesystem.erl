b8fb47eb2b228fc8d760297a7389b3378123e883
Ben <andreabenini@users.noreply.github.com>
2017-03-10 11:52:56 +0100

    Create filesystem.erl

---
+++ src/snippets/filesystem.erl
@@ -0,0 +1,7 @@
+%% MAKE RELATIVE SOFT LINK - Create a relative soft link
+%% @see Erlang does NOT create relative softlink, full path softlink only... this is just an hack
+makeRelativeSoftLink(FullFileNameSrc, FullFileNameDst) ->
+    file:make_symlink(filename:basename(binary_to_list(FullFileNameSrc)),
+                      filename:basename(binary_to_list(FullFileNameDst)) ),
+    file:rename(filename:basename(binary_to_list(DefaultUserDst)),
+                binary_to_list(DefaultUserDst)).
