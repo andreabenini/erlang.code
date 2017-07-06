2b90caad058e7a9be53effdfbf2c94018d55c4bc
Ben <andreabenini@users.noreply.github.com>
2017-07-06 12:17:31 +0200

    Update strings.binaries.erl

--- src/snippets/strings.binaries.erl
+++ src/snippets/strings.binaries.erl
@@ -3,4 +3,9 @@
 [72,101,108,108,111,32,87,111,114,108,100,44,32,114,101,115,117,108,116,32,105,115,32,"42"]
 > lists:flatten(AA).
 "Hello World, result is 42"
- 
+> lists:concat([helloAtom," World, result is ", 42, "\n"]).
+"helloAtom World, result is 42\n"
+
+%% Some binaries
+> erlang:iolist_to_binary(AA).
+<<"Hello World, result is 42">>
