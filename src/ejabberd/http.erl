f22614da7416d1c6de4b179503c55343c39c43f7
Ben <andreabenini@users.noreply.github.com>
2017-11-10 12:45:24 +0100

    Update http.erl

--- src/ejabberd/http.erl
+++ src/ejabberd/http.erl
@@ -7,15 +7,15 @@ request(Domain) ->
     case httpc:request(get, {URL, []}, [{timeout, ?HTTP_REQUEST_TIMEOUT}], [], ?PROFILE) of
         %% OK: Server replied with OK [HTTP: 200..29]
         {ok, {{_HTTPType, ReturnCode, _State}, Headers, BodyMessage}} when ReturnCode >= 200, ReturnCode =< 299 ->
-            ?TRACE("HTTP Reply  ~p, Headers ~p", [ReturnCode, Headers]),
-            ?INFO_MSG("[HTTP:~p] ~p", [ReturnCode, BodyMessage]);
+            io:format("HTTP Reply  ~p, Headers ~p", [ReturnCode, Headers]),
+            io:format("[HTTP:~p] ~p", [ReturnCode, BodyMessage]);
 
         %% ERROR: Server replied with an error
         {ok, {{_HTTPType, ReturnCode, _State}, Headers, BodyMessage}} when ReturnCode < 200; ReturnCode > 299 ->
-            ?ERROR_MSG("[HTTP:~p] Headers ~p", [ReturnCode, Headers]),
-            ?ERROR_MSG("Body       ~p", [BodyMessage]);
+            io:format("ERROR: [HTTP:~p] Headers ~p", [ReturnCode, Headers]),
+            io:format("ERROR: Body       ~p", [BodyMessage]);
 
         %% ERROR: Something else is wrong
         Error ->
-            ?ERROR_MSG("ERROR requesting ~s, Error = ~p", [URL, Error])
+            io:format("ERROR: requesting ~s, Error = ~p", [URL, Error])
     end.
