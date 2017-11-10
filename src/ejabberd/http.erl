9f35e285101e21231ce283582a63c80480e25432
Ben <andreabenini@users.noreply.github.com>
2017-11-10 12:44:10 +0100

    Update http.erl

--- src/ejabberd/http.erl
+++ src/ejabberd/http.erl
@@ -1,9 +1,10 @@
-
+-define(HTTP_REQUEST_TIMEOUT, 30000).
+-define(PROFILE, myOwnProfile).
 
 request(Domain) ->
     URL = "http://www.google.com",      %% URL must be a string, not a binary stream
     %% HTTP Request
-    case httpc:request(get, {URL, []}, [{timeout, ?HTTP_REQUEST_TIMEOUT}], [], httpcClientName(Domain)) of
+    case httpc:request(get, {URL, []}, [{timeout, ?HTTP_REQUEST_TIMEOUT}], [], ?PROFILE) of
         %% OK: Server replied with OK [HTTP: 200..29]
         {ok, {{_HTTPType, ReturnCode, _State}, Headers, BodyMessage}} when ReturnCode >= 200, ReturnCode =< 299 ->
             ?TRACE("HTTP Reply  ~p, Headers ~p", [ReturnCode, Headers]),
