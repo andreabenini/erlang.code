7149396ee1bc34522bccacb33e4264ca3c34b04f
Ben <andreabenini@users.noreply.github.com>
2018-05-23 18:30:31 +0200

    Update spawn.erl

--- src/snippets/spawn.erl
+++ src/snippets/spawn.erl
@@ -1,6 +1,19 @@
+%% spawn/1 -> spawn(fun() -> server("Hello") end). 
+%% spawn/3 -> spawn(moduleName, funName, ["argument", "list"]),  %% ?MODULE
+
+%%
+%% Hello world spawn
+-module(helloworld). 
+-export([start/0]). 
+start() ->
+   spawn(fun() -> server("Hello") end). 
+server(Message) ->
+   io:fwrite("~p",[Message]).
+
+%%
+%% A little bit complex (but not that much)
 -module(spawn). 
 -export([loop/0,start/0]). 
-
 loop() ->
    receive 
       {rectangle, Width, Ht} -> 
@@ -13,7 +26,6 @@ loop() ->
       io:fwrite("Unknown"), 
       loop() 
    end. 
-
 start() ->
    Pid = spawn(fun() -> loop() end), 
    Pid ! {rectangle, 6, 10}.
