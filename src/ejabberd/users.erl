dd826035c03389b69201fa96874f010c0ccaebe6
Ben <andreabenini@users.noreply.github.com>
2018-07-16 17:43:09 +0200

    get vcard information from server

--- src/ejabberd/users.erl
+++ src/ejabberd/users.erl
@@ -3,3 +3,8 @@ mod_last:get_last_info(<<"ben">>, <<"whatever.com">>).
 % {ok,1497632173,<<>>}      %% UNIX Timestamp representing last login
 mod_last:get_last_info(<<"wtf">>, <<"whatever.com">>).
 % not_found                 %% ATOM (user never logged in)
+
+%% Retrieve user vcard basic info
+mod_admin_extra:get_vcard(User, Domain, <<"FN">>)
+%% Retrieve user vcard extended info
+mod_admin_extra:get_vcard(User, Domain, <<"ADR">>, <<"CTRY">>)
